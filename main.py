d848ba30c01edf8d5eeebfa5653d7955b8c51a7a
Ken <54377513+Vinddictive@users.noreply.github.com>
2024-05-21 14:54:16 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
+from aiogram.client.default import DefaultBotProperties
 from aiogram import Bot, Dispatcher, types, F, Router
 from aiogram.enums import ParseMode
+from aiogram.filters import Command
+
 import asyncio
 import os
+from dotenv import load_dotenv
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-bot = Bot(token=BOT_TOKEN)
-form_router = Router()
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+router = Router()
 
 
-@form_router.message(F.text.casefold() == "/start")
+@router.message(Command("start"))
 async def start(message: types.Message):
     await message.answer(f"Send or forward a message, media or file to this bot to retrieve user info.")
 
 
-@form_router.message()
+# Handler for all messages
+@router.message()
 async def handle_media(message: types.Message):
+    # Check if the message is forwarded from a user or a chat
     if message.forward_from:
         user = message.forward_from
     elif message.forward_from_chat:
@@ -22,29 +30,25 @@ async def handle_media(message: types.Message):
     else:
         user = message.from_user
 
+    # If the user is a chat (group, supergroup, or channel)
     if isinstance(user, types.Chat):
         if user.type in ('group', 'supergroup', 'channel'):
-            if user.username:
-                member_count = await bot.get_chat_member_count(user.id)
-                await message.answer(text=f"<b>ID:</b>  <code>{user.id}</code>\n"
-                                          f"<b>Name:</b>  {user.title}\n"
-                                          f"<b>Username:</b>  @{user.username}\n"
-                                          f"<b>Participants:</b>  {member_count}", parse_mode=ParseMode.HTML)
-            else:
-                await message.answer(text=f"<b>ID:</b>  <code>{user.id}</code>\n"
-                                          f"<b>Name:</b>  {user.title}\n"
-                                          f"<b>Username:</b>  <i>UNAVAILABLE</i>\n"
-                                          f"<b>Participants:</b>  <i>UNAVAILABLE</i>", parse_mode=ParseMode.HTML)
+            member_count = await bot.get_chat_member_count(user.id)
+            await message.answer(text=f"<b>ID:</b>  <code>{user.id}</code>\n"
+                                      f"<b>Name:</b>  {user.title}\n"
+                                      f"<b>Username:</b>  @{user.username if user.username else 'UNAVAILABLE'}\n"
+                                      f"<b>Participants:</b>  {member_count if user.username else 'UNAVAILABLE'}")
     else:
+        # For individual users, display their ID, full name, username, and a link to their profile
         await message.answer(text=f"<b>ID:</b>  <code>{user.id}</code>\n"
                                   f"<b>Name:</b>  {user.full_name}\n"
                                   f"<b>Username:</b>  @{user.username}\n"
-                                  f"<b>Link:</b>  <a href='tg://user?id={user.id}'>🔗 {user.full_name}</a>", parse_mode=ParseMode.HTML)
+                                  f"<b>Link:</b>  <a href='tg://user?id={user.id}'>🔗 {user.full_name}</a>")
 
 
 async def main():
     dp = Dispatcher()
-    dp.include_router(form_router)
+    dp.include_router(router)
     await dp.start_polling(bot)
 
 
